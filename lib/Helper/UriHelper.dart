6ee66e0fa72d6435f108e28e018c7f18e620eec1
TuanDatCoder <tuandatdq03@gmail.com>
2025-07-25 02:33:34 +0700

    fetch view project for homepage

--- lib/Helper/UriHelper.dart
+++ lib/Helper/UriHelper.dart
@@ -1,5 +1,7 @@
 class UriHelper {
-  static const String _baseUrl = 'https://10.0.2.2:7128/api';
+  //static const String _baseUrl = 'https://10.0.2.2:7128/api';
+
+  static const String _baseUrl = 'https://intellipm-c5c2a5athaa2b9cp.southeastasia-01.azurewebsites.net/api';
 
   static Uri build(String endpoint) {
     return Uri.parse('$_baseUrl$endpoint');
